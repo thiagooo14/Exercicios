400847ce9ed6c470c4420b7d845e628f711ad60a
thiagooo14 <thiagooo15@gmail.com>
2021-03-24 18:58:39 -0300

    req 11

--- exercicios.js
+++ exercicios.js
@@ -147,3 +147,36 @@ if (valorCusto >= 0 && valorVenda >= 0) {
 } else {
   console.log('ERRO: não utilize numero negativo');
 }
+// 11
+console.log('exercicio 11');
+let aliquotINSS;
+let aliquotIR;
+
+let grossSalary = 3500.00;
+
+if (grossSalary <= 1556.94) {
+  aliquotINSS = grossSalary * 0.08;
+} else if (grossSalary <= 2594.92) {
+  aliquotINSS = grossSalary * 0.09;
+} else if (grossSalary <= 5189.82) {
+  aliquotINSS = grossSalary * 0.11;
+} else {
+  aliquotINSS = 570.88;
+}
+
+let baseSalary = grossSalary - aliquotINSS;
+
+if (baseSalary <= 1903.98) {
+  aliquotIR = 0;
+} else if (baseSalary <= 2826.65) {
+  aliquotIR = (baseSalary * 0.075) - 142.80;
+} else if (baseSalary <= 3751.05) {
+  aliquotIR = (baseSalary * 0.15) - 354.80;
+} else if (baseSalary <= 4664.68) {
+  aliquotIR = (baseSalary * 0.225) - 636.13;
+} else {
+  aliquotIR = (baseSalary * 0.275) - 869.36;
+};
+
+console.log("Salário: " + (baseSalary - aliquotIR));
+
